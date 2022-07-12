b1226c4eb3d657a9cde4b13880e42a3bdf004004
rockingerser <100497322+rockingerser@users.noreply.github.com>
2022-07-11 23:02:32 -0500

    Delete js/modules directory

--- js/modules/agents.js
+++
@@ -1,7 +0,0 @@
-var agentHTML = `
-    <img class="wolfoo-character-avatar non-grabbable" draggable="false">
-    <span class="wolfoo-character-name non-grabbable"></span>
-    <progress class="wolfoo-character-health" value="1"></progress>
-`;
-// Is not necessary to add "non-grabbable" class on progress element because i disabled grabbing on these, because if agents try to grab one of these, the script crashes
-export default agentHTML;
